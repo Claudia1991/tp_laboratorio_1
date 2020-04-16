1c1da5ada8d4065cf6b435f5fcdfa469e6fa850f
Claudia <serbe_03@hotmail.com>
2020-04-16 20:51:40 -0300

    Modificaciones de comentarios

--- TrabajoPractico01/src/funciones_calculadora.c
+++ TrabajoPractico01/src/funciones_calculadora.c
@@ -3,12 +3,6 @@
 #include "defines.h"
 #include "utn.h"
 
-/**
- * \brief Realiza el factorial de un numero entero.
- * \param numero Es el numero del cual se quiere el factorial.
- * \return Retorna el factorial
- *
- * */
 long int factorial(int numero) {
 	if (numero == 0) {
 		return 1;
@@ -17,34 +11,14 @@ long int factorial(int numero) {
 	}
 }
 
-/**
- * \brief Verifica si un numero es negativo o no.
- * \param numero Es el numero a saber si es negativo o no.
- * \return 1 si es negativo, 0 si es cero o positivo
- *
- * */
 int esNegativo(int numero) {
 	return numero < 0;
 }
 
-/**
- * \brief Verifica si un numero es cero o no.
- * \param numero Es el numero a saber si es cero o no.
- * \return 1 si es cero, 0 en cualquier otro caso.
- *
- * */
 int esCero(int numero) {
 	return numero == 0;
 }
 
-/**
- * \brief Realiza la division y guarda el resultado en un puntero.
- * \param *pResultado Es el puntero donde se guarda el resultado de la division.
- * \param dividendo Es el dividendo
- * \param divisor Es el divisor.
- * \return -1 (RESULTADO_NO_OK) si da error, 0 (RESULTADO_OK) si se realizo la division
- *
- * */
 int dividir(float *pResultado, int dividendo, int divisor) {
 	if (esCero(divisor)) {
 		return RESULTADO_NO_OK;
@@ -54,13 +28,6 @@ int dividir(float *pResultado, int dividendo, int divisor) {
 	}
 }
 
-/**
- * \brief Realiza el factorial y guarda el resultado en un puntero.
- * \param *pResultado Es el puntero donde se guarda el resultado del factorial.
- * \param numero Es el numero a cualcular el factorial.
- * \return -1 (RESULTADO_NO_OK) si da error, 0 (RESULTADO_OK) si se realizo el factorial.
- *
- * */
 int realizarFactorial(long int *pResultado, int numero) {
 	if (esNegativo(numero)) {
 		return RESULTADO_NO_OK;
@@ -70,25 +37,14 @@ int realizarFactorial(long int *pResultado, int numero) {
 	}
 }
 
-/**
- * \brief Muestra la bienvenida al usuario.
- * \param No recibe parametros.
- * \return No retorna nada.
- *
- * */
+
 void mostrarBienvenida() {
 	printf("--------------------\n");
 	printf("BIENVENIDO A LA SUPER CALCULADORA\n");
 	printf("--------------------\n");
 }
 
-/**
- * \brief Muestra en pantalla las opciones del usuario.
- * \param pResultadoOpcion Es el puntero que almacena si el usuario ingreso bien los datos.
- * \param pOpcion Es el puntero donde se almacena la opcion que escogio el usuario.
- * \return No retorna nada.
- *
- * */
+
 void mostrarMenuOpciones(int *pResultadoOpcion, int *pOpcion) {
 	system("CLS()");
 	printf("Opciones disponibles: \n");
@@ -113,12 +69,7 @@ void mostrarMenuOpciones(int *pResultadoOpcion, int *pOpcion) {
 	REINTENTOS);
 }
 
-/**
- * \brief Muestra el mensaje de despedida y cierra la aplicacion.
- * \param No recibe parametros.
- * \return No retorna nada.
- *
- * */
+
 void salir() {
 	printf("--------------------\n");
 	printf("GRACIAS POR USAR A LA SUPER CALCULADORA\n");
@@ -126,33 +77,13 @@ void salir() {
 	exit(0);
 }
 
-/**
- * \brief Valida que ambos numeros fueron ingresados por el usuario
- * \param flagIngresoPrimerOperando Bandera si el usuario ingreso el primer numero.
- * \param flagIngresoSegundoOperando Bandera si el usuario ingreso el segundo numero.
- * \return Retorna 1 si los dos numeros fueron ingresados, 0 en cualquier otro caso.
- *
- * */
+
 int validarIngresoOperandos(int flagIngresoPrimerOperando,
 		int flagIngresoSegundoOperando) {
 	return flagIngresoPrimerOperando && flagIngresoSegundoOperando;
 }
 
-/**
- * \brief Realiza todas las operaciones con los numeros ingresados por el usuario.
- * \param flagIngresoPrimerOperando Bandera si el usuario ingreso el primer numero.
- * \param flagIngresoSegundoOperando Bandera si el usuario ingreso el segundo numero.
- * \param numeroUno Es el primer numero ingresado por el usuario.
- * \param numeroDos Es el segundo numero ingresado por el usuario.
- * \param pSuma Es el puntero donde se guarda el resultado de la suma.
- * \param pResta Es el puntero donde se guarda el resultado de la resta.
- * \param pMultiplicacion Es el puntero donde se guarda el resultado de la multiplicacion.
- * \param pDivision Es el puntero donde se guarda el resultado de la division.
- * \param pFactorialPrimerOperando Es el puntero donde se guarda el resultado del factorial del primer numero.
- * \param pFactorialSegundoOperando Es el puntero donde se guarda el resultado del factorial del segundo numero.
- * \return RESULTADO_OK(0) si se pudo realizar todas las operaciones, RESULTADO_NO_OK(-1) si hubo algun error.
- *
- * */
+
 int realizarOperaciones(int flagIngresoPrimerOperando,
 		int flagIngresoSegundoOperando, int numeroUno, int numeroDos,
 		int *pSuma, int *pResta, long int *pMultiplicacion, float *pDivision,
@@ -193,21 +124,7 @@ int realizarOperaciones(int flagIngresoPrimerOperando,
 	return resultado;
 }
 
-/**
- * \brief Muestra los resultados por pantalla si se cargaron los dos numeros, sino muestra un mensaje de error.
- * \param flagIngresoPrimerOperando Bandera si el usuario ingreso el primer numero.
- * \param flagIngresoSegundoOperando Bandera si el usuario ingreso el segundo numero.
- * \param numeroUno Es el primer numero ingresado por el usuario.
- * \param numeroDos Es el segundo numero ingresado por el usuario.
- * \param suma Es el resultado de la suma.
- * \param resta Es  el resultado de la resta.
- * \param multiplicacion Es  el resultado de la multiplicacion.
- * \param division Es el resultado de la division.
- * \param factorialPrimerOperando Es el resultado del factorial del primer numero.
- * \param factorialSegundoOperando Es el resultado del factorial del segundo numero.
- * \return No retorna nada.
- *
- * */
+
 void mostrarResultados(int flagIngresoPrimerOperando,
 		int flagIngresoSegundoOperando, int numeroUno, int numeroDos, int suma,
 		int resta, long int multiplicacion, float division,
--- TrabajoPractico01/src/utn.h
+++ TrabajoPractico01/src/utn.h
@@ -1,20 +1,84 @@
 #ifndef UTN_H_
 #define UTN_H_
 
+/**
+ * \brief Pide al usuario un numero, con un minimo y maximo segun la cantidad de reintentos.
+ * \param pResultado Es el puntero donde se guardar el numero ingresado.
+ * \param mensaje Es el mensaje que se muestra al usuario pidiendole que ingrese un dato.
+ * \param mensajeError Es el mensaje que se muestra en caso de un error.
+ * \param minimo Es el minimo valor aceptado.
+ * \param maximo Es el maximo valor aceptado.
+ * \param reintentos Es la cantidad de veces que el usuario se puede equivocar.
+ * \return 0 si Ok o -1 para indicar un error
+ *
+ * */
 int utn_getNumero(int *pResultado, char *mensaje, char *mensajeError, int minimo, int maximo, int reintentos);
 
+/**
+ * \brief Pide al usuario un caracter, con un minimo y maximo segun la cantidad de reintentos.
+ * \param pResultado Es el puntero donde se guardar el caracter ingresado.
+ * \param mensaje Es el mensaje que se muestra al usuario pidiendole que ingrese un dato.
+ * \param mensajeError Es el mensaje que se muestra en caso de un error.
+ * \param minimo Es el minimo valor aceptado.
+ * \param maximo Es el maximo valor aceptado.
+ * \param reintentos Es la cantidad de veces que el usuario se puede equivocar.
+ * \return 0 si Ok o -1 para indicar un error
+ *
+ * */
 int utn_getCaracter(char* pResultado, char* mensaje,char* mensajeError, char minimo,char maximo,int reintentos);
 
+/**
+ * \brief Pide al usuario un numero, con un minimo y maximo segun la cantidad de reintentos.
+ * \param pResultado Es el puntero donde se guardar el numero ingresado.
+ * \param mensaje Es el mensaje que se muestra al usuario pidiendole que ingrese un dato.
+ * \param mensajeError Es el mensaje que se muestra en caso de un error.
+ * \param reintentos Es la cantidad de veces que el usuario se puede equivocar.
+ * \return 0 si Ok o -1 para indicar un error
+ *
+ * */
 int utn_getNumeroSinMinMax(int *pResultado, char *mensaje, char *mensajeError, int reintentos);
 
+/**
+ * \brief Ordena un array de enteros DESC
+ * \param pArray Es el puntero al array a ser ordenado
+ * \param size Es la longitud del array
+ * \return Cantidad de iteraciones necesarias para ordenar si Ok o -1 para indicar un error
+ *
+ */
 int ordenarArrayInt(int* pArray, int limite);
 
+/**
+ * \brief Genera un numero aleatorio
+ * \param desde Numero aleatorio munimo
+ * \param hasta Numero aleatorio muximo
+ * \param iniciar Indica si se trata del primer numero solicitado 1 indica que si
+ * \return retorna el numero aleatorio generado
+ *
+ */
 char getNumeroAleatorio(int desde , int hasta, int iniciar);
 
+/**
+ * \brief Verifica si el valor recibido contiene solo letras
+ * \param str Array con la cadena a ser analizada
+ * \return 1 si contiene solo ' ' y letras y 0 si no lo es
+ *
+ */
 int esSoloLetras(char str[]);
 
+/**
+ * \brief Verifica si el valor recibido contiene solo letras y numeros
+ * \param str Array con la cadena a ser analizada
+ * \return 1 si contiene solo espacio o letras y numeros, y 0 si no lo es
+ *
+ */
 int esAlfaNumerico(char str[]);
 
+/**
+ * \brief Verifica si el valor recibido contiene solo numeros, + y -
+ * \param str Array con la cadena a ser analizada
+ * \return 1 si contiene solo numeros, espacios y un guion.
+ *
+ */
 int esTelefono(char str[]);
 
 #endif /* UTN_H_ */
